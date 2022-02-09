2a79281a589dd98ec9389d96994807eb018224f6
Michael McShinsky <mmcshinsky@gmail.com>
2022-02-09 12:11:12 -0700

    remove github

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
-const isProd = process.env.NODE_ENV === "production";
-
 module.exports = {
   i18n: {
     locales: ["en-US", "es-ES"],
     defaultLocale: "en-US",
   },
   reactStrictMode: true,
-  assetPrefix: isProd ? "/ravenproject.github.io/" : "",
 };
