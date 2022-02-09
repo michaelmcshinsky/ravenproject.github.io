2b464eff430a6ea0eb5449bdb70327e2c279b015
Michael McShinsky <mmcshinsky@gmail.com>
2022-02-09 11:49:29 -0700

    Updates for github pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
   i18n: {
     locales: ["en-US", "es-ES"],
     defaultLocale: "en-US",
   },
   reactStrictMode: true,
-}
+  assetPrefix: isProd ? "/ravenproject.github.io/" : "",
+};
